2dde99410dd10cbdf3f5f0416269fd8a4e7a7521
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-09-16 19:30:49 +0100

    cron job to clean old docker images and containers

--- src/clustermap_mesos/servers/docker.clj
+++ src/clustermap_mesos/servers/docker.clj
@@ -15,4 +15,7 @@
                                                     :key-server "keyserver.ubuntu.com"
                                                     :key-id "36A1D7869245C8950F966E92D8576A8BA88D21E9"})
                 (package-manager :update)
-                (package "lxc-docker"))}))
+                (package "lxc-docker")
+
+                (remote-file "/usr/local/bin/docker-clean" :local-file "resources/files/docker/docker-clean" :mode "755")
+                (remote-file "/etc/cron.d/docker-clean" :content "01 03 * * * root /usr/local/bin/zookeeper-clean")))}))
