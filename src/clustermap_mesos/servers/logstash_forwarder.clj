589634b8d7e2761d88995324247a7287e2eb985f
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-09-17 20:17:40 +0100

    add encryption back to lumberjack... doesn't work without

--- src/clustermap_mesos/servers/logstash_forwarder.clj
+++ src/clustermap_mesos/servers/logstash_forwarder.clj
@@ -24,6 +24,11 @@
                 (exec-script* "cd /opt/logstash-forwarder-0.3.1 ; if ! test -e lumberjack_0.3.1_amd64.deb ; then PATH=/usr/local/bin:$PATH make deb ; dpkg -i lumberjack_0.3.1_amd64.deb ;fi")
 
                 ;; configure and start
+                (directory "/etc/pki/tls/certs" :action :create)
+                (remote-file "/etc/pki/tls/certs/logstash-forwarder.crt" :local-file "resources/files/logstash_forwarder/certs/logstash-forwarder.crt")
+                (directory "/etc/pki/tls/private" :action :create)
+                (remote-file "/etc/pki/tls/private/logstash-forwarder.key" :local-file "resources/files/logstash_forwarder/certs/logstash-forwarder.key")
+
                 (remote-file "/etc/lumberjack.conf" :local-file "resources/files/logstash_forwarder/lumberjack.conf")
                 (remote-file "/etc/init/lumberjack.conf" :local-file "resources/files/logstash_forwarder/upstart_lumberjack.conf")
                 (exec-script* "update-rc.d -f lumberjack remove")
