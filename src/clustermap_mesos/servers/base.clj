4a92218b7cde5a783ea019db61f02c75d449e68a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-09-17 15:07:15 +0100

    add openntpd to all servers

--- src/clustermap_mesos/servers/base.clj
+++ src/clustermap_mesos/servers/base.clj
@@ -1,7 +1,7 @@
 (ns clustermap-mesos.servers.base
   (:require
    [pallet.api :refer [server-spec plan-fn]]
-   [pallet.actions :refer [package]]
+   [pallet.actions :refer [package exec-script*]]
    [pallet.crate.automated-admin-user :refer [automated-admin-user]]))
 
 (defn  base-server
@@ -10,6 +10,7 @@
   (server-spec
    :phases
    {:bootstrap (plan-fn
-                (automated-admin-user))
+                (automated-admin-user)
+                (exec-script* "locale-gen en_GB.UTF-8 en_US.UTF-8"))
     :configure (plan-fn
                 (package "openntpd"))}))
