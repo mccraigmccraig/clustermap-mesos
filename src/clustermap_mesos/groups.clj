3f201a2ed5e82ecf374205c140c6b773deb208a6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-10-15 13:55:14 +0100

    drop ES heap size now we're using doc_values

--- src/clustermap_mesos/groups.clj
+++ src/clustermap_mesos/groups.clj
@@ -26,7 +26,7 @@
   (group-spec "mesos-data-slave"
               :extends [(base-server)
                         (mesos-slave-server)
-                        (elasticsearch-data-server "clustermap" "3g")]
+                        (elasticsearch-data-server "clustermap" "2g")]
     :node-spec (eu-west-ubuntu-1404-pv-ebs-node "m3.large" "eu-west-1c" "subnet-c9ece28f" "sg-8c2a86e9" "mccraigkey" "cmap2-appserver")
     ;; :count 3
     ))
