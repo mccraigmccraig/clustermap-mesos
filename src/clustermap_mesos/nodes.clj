739bc7d492105d7429634516ed05ba38d2ae207e
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-01-06 20:02:32 +0000

    don't need PV virtualization on AWS anymore

--- src/clustermap_mesos/nodes.clj
+++ src/clustermap_mesos/nodes.clj
@@ -3,7 +3,7 @@
    [pallet.api :refer [node-spec]]))
 
 (defn eu-west-ubuntu-1404-hvm-ebs-node
-  [{:keys [hardware location subnet-id security-group-id key-name iam-instance-profile-name]}]
+  [{:keys [hardware location subnet-id security-group-id key-name iam-instance-profile-name volume-size]}]
   (node-spec
    :image {:image-id "ami-96c41ce1";; alestic eu-west-1 ubuntu 14.04 HVM EBS-SSD
            :os-family :ubuntu
@@ -22,32 +22,32 @@
                                      :associate-public-ip-address true
                                      :delete-on-termination true}]
                :block-device-mappings [{:device-name "/dev/sda1"
-                                        :ebs {:volume-size 100
+                                        :ebs {:volume-size volume-size
                                               :delete-on-termination true
                                               :volume-type "gp2"}}]
                :iam-instance-profile {:name iam-instance-profile-name}}}))
 
-(defn eu-west-ubuntu-1404-pv-ebs-node
-  [{:keys [hardware location subnet-id security-group-id key-name iam-instance-profile-name]}]
-  (node-spec
-   :image {:image-id "ami-aec41cd9"  ;; alestic eu-west-1 ubuntu 14.04 PV EBS-SSD
-           :os-family :ubuntu
-           :os-version "14.04"
-           :os-64-bit true
-           :login-user "ubuntu"
-           :key-name key-name}
-   :hardware {:hardware-id hardware}
-   :location {:location-id location}
-   :provider {
-              :pallet-ec2
-              {
-               :network-interfaces [{:device-index 0
-                                     :subnet-id subnet-id
-                                     :groups [security-group-id]
-                                     :associate-public-ip-address true
-                                     :delete-on-termination true}]
-               :block-device-mappings [{:device-name "/dev/sda1"
-                                        :ebs {:volume-size 200
-                                              :delete-on-termination true
-                                              :volume-type "gp2"}}]
-               :iam-instance-profile {:name iam-instance-profile-name}}}))
+;; (defn eu-west-ubuntu-1404-pv-ebs-node
+;;   [{:keys [hardware location subnet-id security-group-id key-name iam-instance-profile-name]}]
+;;   (node-spec
+;;    :image {:image-id "ami-aec41cd9"  ;; alestic eu-west-1 ubuntu 14.04 PV EBS-SSD
+;;            :os-family :ubuntu
+;;            :os-version "14.04"
+;;            :os-64-bit true
+;;            :login-user "ubuntu"
+;;            :key-name key-name}
+;;    :hardware {:hardware-id hardware}
+;;    :location {:location-id location}
+;;    :provider {
+;;               :pallet-ec2
+;;               {
+;;                :network-interfaces [{:device-index 0
+;;                                      :subnet-id subnet-id
+;;                                      :groups [security-group-id]
+;;                                      :associate-public-ip-address true
+;;                                      :delete-on-termination true}]
+;;                :block-device-mappings [{:device-name "/dev/sda1"
+;;                                         :ebs {:volume-size 200
+;;                                               :delete-on-termination true
+;;                                               :volume-type "gp2"}}]
+;;                :iam-instance-profile {:name iam-instance-profile-name}}}))
